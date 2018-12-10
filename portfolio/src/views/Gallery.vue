741dbb028d92eba7390d8de4b1227108a235bae8
Manuel Graf <mg@apfelkuh.de>
2018-12-10 12:05:10 +0100

    globe

--- portfolio/src/views/Gallery.vue
+++ portfolio/src/views/Gallery.vue
@@ -1,5 +1,12 @@
 <template>
-  <div id="gallery" class="view view--gallery"></div>
+  <div id="gallery" class="view view--gallery">
+    <div class="view__content">
+      <demo-placeholder>
+        <h4 slot="placeholder">3D Data Visualization </h4>
+        <globe-scene slot="demo"></globe-scene>
+      </demo-placeholder> 
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
@@ -10,4 +17,16 @@
     background: $bgcolor-gallery;
   }
 </style>
+
+<script>
+import DemoPlaceholder from '@/components/DemoPlaceholder'
+import GlobeScene from '@/components/GlobeScene'
+export default {
+  components:{
+    DemoPlaceholder,
+    GlobeScene
+  }
+}
+</script>
+
 
