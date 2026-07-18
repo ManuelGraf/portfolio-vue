14211a7facf837bd7eef5518cbebeb3a785a1148
Manuel Graf <mg@apfelkuh.de>
2026-07-18 06:29:01 +0200

    logo size desktop

--- portfolio/src/components/Logo.vue
+++ portfolio/src/components/Logo.vue
@@ -3,32 +3,21 @@
     <div class="logo-flex">
       <router-link to="/">
         <div class="logo-icon">
-          <svg
-            version="1.1"
-            id="Layer_1"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            x="0px"
-            y="0px"
-            width="65.633px"
-            height="38.6px"
-            viewBox="113.7 230.7 65.633 38.6"
-            enable-background="new 113.7 230.7 65.633 38.6"
-            xml:space="preserve"
-          >
-            <path
-              class="eye"
+          <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+            x="0px" y="0px" width="100px" height="38.6px" viewBox="113.7 230.7 65.633 38.6"
+            enable-background="new 113.7 230.7 65.633 38.6" xml:space="preserve">
+            <path class="eye"
               d="M166.7,261.9l-4.9-4.9l-4.1-4.1l0,0l-5.4,5.399l1.5,1.5l2.6,2.601c-3,1.399-6.4,1.5-9.4,0.199
               c-1.3-0.6-2.6-1.3-3.6-2.399c-1.5-1.5-2.5-3.3-3-5.3c-0.6-2.301-0.5-4.8,0.4-7.101l4.4,1.2l4.5,1.2l-1.2-4.5l-1.2-4.4
               c4-1.6,8.6-0.8,11.9,2.2l5-5c5.2,2.8,9.4,6.5,12,9.2c-3.9-5.8-13.1-17-25.7-17c-17,0-36.8,19.3-36.8,19.3s19.8,19.3,36.8,19.3
               c12.4,0,21.4-9.7,25.5-15C173.8,256.6,170.6,259.4,166.7,261.9z M134.4,260.8c0.3,0.601,0.7,1.3,1.1,1.9
-              c-8.2-4.8-13.7-11.7-13.7-11.7s7.6-9.5,18.2-13.9l1.3,4.9l-5.2-1.4c-1.8,2.3-2.9,5-3.5,7.7C131.8,252.5,132.4,256.9,134.4,260.8z"
-            ></path>
+              c-8.2-4.8-13.7-11.7-13.7-11.7s7.6-9.5,18.2-13.9l1.3,4.9l-5.2-1.4c-1.8,2.3-2.9,5-3.5,7.7C131.8,252.5,132.4,256.9,134.4,260.8z">
+            </path>
           </svg>
         </div>
       </router-link>
       <h1 class="logo-name">
-        <span class="first-name">manuel</span>
+        <span class="first-name">manuel.</span>
         <span class="last-name">graf</span>
       </h1>
       <h2 class="subline">
@@ -65,24 +54,39 @@
   .logo-name {
     text-align: center;
     font-size: $fontsize-xl;
+    padding-top: 1rem;
+
+    @include viewport-tablet {
+      font-size: $fontsize-xxl;
+    }
   }
+
   .subline {
-    padding-top: 0.8rem;
+    padding-top: 2rem;
     color: $color-black-light;
     font-size: $fontsize-s;
+
+    @include viewport-tablet {
+      font-size: $fontsize-m;
+    }
   }
+
   .subline-graf {
     color: $color-green;
   }
+
   .first-name {
     color: $color-black-light;
   }
+
   .last-name {
-    color: $color-green;
+    color: $color-black-light;
+    // color: $color-green;
   }
 
   .logo-icon {
     order: 1;
+
     // transition: all $duration-complex ease;
     svg {
       height: 60px;
@@ -91,6 +95,7 @@
       // transition: all $duration-complex ease;
     }
   }
+
   .eye {
     fill: $color-black-light;
     transition: fill $duration-noticeable ease;
@@ -115,22 +120,27 @@
   .subline {
     display: none;
   }
+
   .subline {
     display: none;
   }
+
   .logo-name {
-    display:none;
+    display: none;
     font-size: $fontsize-s;
     padding: 2px 0 0 0;
     margin: 0;
+
     .first-name,
     .last-name {
       // font-weight:300;
       color: $color-white-darkest;
     }
   }
+
   .logo-icon {
     width: 45px;
+
     svg {
       height: 20px;
       padding: 0;
@@ -144,9 +154,8 @@
 export default {
   name: "logo",
   components: {},
-  data: function() {
+  data: function () {
     return {};
   }
 };
 </script>
-  
